0083ea4bf4c92a5354d678a46413ef61b31f0df3
moxin <244822903@qq.com>
2021-09-12 13:12:10 +0800

    修复类型冲突

--- src/components/RightRender.tsx
+++ src/components/RightRender.tsx
@@ -1,29 +1,26 @@
-import React, { FC } from 'react';
+import React from 'react';
 import styles from './styles.less';
 import { Avatar, Dropdown, message } from 'antd';
 import Menulist from './Menulist';
-import { history, UserModelState, ConnectProps, Loading, connect } from 'umi';
+import { history, UserModelState, Loading, connect } from 'umi';
 
-interface PageProps extends ConnectProps {
-  userModels: UserModelState;
-  loading: boolean;
-  dispatch: any;
-}
-const RightRender: FC<PageProps> = ({ dispatch }) => {
+const RightRender = (props: any) => {
   let name = '陌心';
   const onClick = ({ key }: any): void => {
     message.destroy();
     if (key === '1') {
       history.push('/PersonalInformation');
     } else if (key === '3') {
-      dispatch({
-        type: 'userModels/getLogOut',
-        payload: null,
-      }).then((res: any) => {
-        if (res.success) {
-          history.push('/Login');
-        }
-      });
+      props
+        .dispatch({
+          type: 'userModels/getLogOut',
+          payload: null,
+        })
+        .then((res: any) => {
+          if (res.success) {
+            history.push('/Login');
+          }
+        });
     } else {
       message.info(`Click on item ${key}`);
     }
